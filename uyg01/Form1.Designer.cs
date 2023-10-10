5d752c52ba1f5c4d6aa832ce3c15461d61f10257
Abdulkadir KOCER <akocerweb@hotmail.com>
2023-10-10 14:06:16 +0300

    Add project files.

---
+++ uyg01/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace uyg01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lbSonuc = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 26F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(185, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(397, 70);
+            label1.TabIndex = 0;
+            label1.Text = "Hesap Programı";
+            // 
+            // lbSonuc
+            // 
+            lbSonuc.AutoSize = true;
+            lbSonuc.Location = new Point(322, 282);
+            lbSonuc.Name = "lbSonuc";
+            lbSonuc.Size = new Size(59, 25);
+            lbSonuc.TabIndex = 1;
+            lbSonuc.Text = "label2";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lbSonuc);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label lbSonuc;
+    }
+}
---
+++ uyg01/Form1.cs
@@ -0,0 +1,21 @@
+namespace uyg01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
